9944b08825527e715f2f8f382f9a9429152ac26a
Ryan Walker <rwalker+MIG@mercuryinsurance.com>
2025-05-15 16:29:01 -0700

    finish scenario implementation

--- src/components/testscenario/AddTestScenario.jsx
+++ src/components/testscenario/AddTestScenario.jsx
@@ -1,37 +1,128 @@
-import { Box, Button } from "@mui/material";
+import {
+  Dialog,
+  Paper,
+  Box,
+  Button,
+  Typography,
+  Grid,
+  Input,
+} from "@mui/material";
+import { useState } from "react";
+import RequestInfoFields from "./RequestInfoFields";
+import ValidationFields from "./ValidationFields";
+import Transition from "../transistions/Transistion";
 
-function AddTestScenario({ onCreate, onClose }) {
+function AddTestScenario({ onClose, onCreate }) {
+    const [formData, setFormData] = useState({
+    scenario_collection: "",
+    data_id: "",
+    description: "",
+    request_info: {
+      type: "POST",
+      body: "",
+      parameters: "",
+      service: "",
+      content_type: "",
+      headers: {
+        Accept: "",
+        "Content-Type": "",
+      },
+    },
+    validation: {
+      status_code: 200,
+      actions: [
+        {
+          path: "",
+          message: "",
+          type: "",
+          mode: "",
+          expected: "",
+          ignore_fields: [],
+        },
+      ],
+    },
+  });
+
+
+  const handleSubmit = (e) => {
+    setFormData(formData)
+    onCreate(formData)
+    e.stopPropagation() 
+  };
+
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        zIndex: 1300,
-        backgroundColor: "rgba(0,0,0,0.5)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <Dialog fullScreen open={true} keepMounted TransitionComponent={Transition}>
+      {/* Centering wrapper */}
       <Box
         sx={{
-          p: 4,
-          backgroundColor: "white",
-          borderRadius: 2,
-          boxShadow: 3,
-          width: "90%",
-          maxWidth: 800,
-          maxHeight: "90vh",
-          overflowY: "auto",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.05)",
         }}
       >
-        <Button onClick={onCreate}>Create</Button>
-        <Button onClick={onClose}>Close</Button>
+        <Paper
+          elevation={4}
+          sx={{
+            p: 4,
+            width: "90%",
+            maxWidth: 600,
+            borderRadius: 2,
+          }}
+        >
+          <Box component="form" onSubmit={handleSubmit}>
+            <Typography variant="h6">Scenario Form</Typography>
+            <Grid container spacing={2}>
+              <Grid item size={{ xs: 4 }}>
+                <Input
+                  name="scenario_collection"
+                  value={formData.scenario_collection}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      scenario_collection: e.target.value,
+                    })
+                  }
+                  placeholder="Scenario Collection"
+                />
+              </Grid>
+              <Grid item size={{ xs: 4 }}>
+                <Input
+                  name="data_id"
+                  value={formData.data_id}
+                  onChange={(e) =>
+                    setFormData({ ...formData, data_id: e.target.value })
+                  }
+                  placeholder="Data ID"
+                />
+              </Grid>
+              <Grid item size={{ xs: 4 }}>
+                <Input
+                  name="description"
+                  value={formData.description}
+                  onChange={(e) =>
+                    setFormData({ ...formData, description: e.target.value })
+                  }
+                  placeholder="Description"
+                />
+              </Grid>
+            </Grid>
+
+            {/* Split into subcomponents */}
+            <RequestInfoFields formData={formData} setFormData={setFormData} />
+            <ValidationFields formData={formData} setFormData={setFormData} />
+
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+            <Button variant="contained" onClick={onClose}>
+              Cancel
+            </Button>
+          </Box>
+        </Paper>
       </Box>
-    </Box>
+    </Dialog>
   );
 }
 
