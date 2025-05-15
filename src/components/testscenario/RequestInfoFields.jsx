0b50586162181ae771675ec4e172173b265da964
Ryan Walker <rwalker+MIG@mercuryinsurance.com>
2025-05-15 16:58:43 -0700

    return mui box with padding

--- src/components/testscenario/RequestInfoFields.jsx
+++ src/components/testscenario/RequestInfoFields.jsx
@@ -1,4 +1,4 @@
-import { TextField, Grid, Typography } from "@mui/material";
+import { Box, TextField, Grid, Typography } from "@mui/material";
 import RequestType from "./request-info/RequestType";
 import ContentType from "./request-info/ContentType";
 import HeadersFields from "./request-info/HeaderFields";
@@ -16,46 +16,52 @@ export default function RequestInfoFields({ formData, setFormData }) {
   };
 
   return (
-    <Grid container spacing={2}>
-      <Grid item size={{ xs: 12 }}>
-        <Typography variant="h3">Request Info</Typography>
+    <Box
+      sx={{
+        p: 3,
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item size={{ xs: 12 }}>
+          <Typography variant="h5">Request Info</Typography>
+        </Grid>
+        <Grid item size={{ xs: 6 }}>
+          <TextField
+            fullWidth
+            name="service"
+            label="Service"
+            value={formData.request_info.service}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item size={{ xs: 6 }}>
+          <RequestType />
+        </Grid>
+        <Grid item size={{ xs: 6 }}>
+          <ContentType />
+        </Grid>
+        <Grid item size={{ xs: 6 }}>
+          <TextField
+            fullWidth
+            name="parameters"
+            label="parameters"
+            value={formData.request_info.parameters}
+            onChange={handleChange}
+          ></TextField>
+        </Grid>
+        <Grid item size={{ xs: 12 }}>
+          <HeadersFields formData={formData} setFormData={setFormData} />
+        </Grid>
+        <Grid item size={{ xs: 12 }}>
+          <TextField
+            fullWidth
+            name="body"
+            label="t"
+            value={formData.request_info.body}
+            onChange={handleChange}
+          />
+        </Grid>
       </Grid>
-      <Grid item size={{ xs: 6 }}>
-        <TextField
-          fullWidth
-          name="service"
-          label="Service"
-          value={formData.request_info.service}
-          onChange={handleChange}
-        />
-      </Grid>
-      <Grid item size={{ xs: 6 }}>
-        <RequestType />
-      </Grid>
-      <Grid item size={{ xs: 6 }}>
-        <ContentType />
-      </Grid>
-      <Grid item size={{ xs: 6 }}>
-        <TextField
-          fullWidth
-          name="parameters"
-          label="parameters"
-          value={formData.request_info.parameters}
-          onChange={handleChange}
-        ></TextField>
-      </Grid>
-      <Grid item size={{ xs: 12}}>
-        <HeadersFields formData={formData} setFormData={setFormData}/>
-      </Grid>
-      <Grid item size={{ xs: 12 }}>
-        <TextField
-          fullWidth
-          name="body"
-          label="t"
-          value={formData.request_info.body}
-          onChange={handleChange}
-        />
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
