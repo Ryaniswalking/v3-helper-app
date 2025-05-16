d9621470843d9ef616cb9dad600a462eb826c9a6
Ryan Walker <rwalker+MIG@mercuryinsurance.com>
2025-05-16 10:52:11 -0700

    add testClass and propagate update

--- src/components/testcase/GlobalInputs.jsx
+++ src/components/testcase/GlobalInputs.jsx
@@ -1,48 +1,68 @@
-import { useState } from "react";
 import { Grid, TextField } from "@mui/material";
-export default function GlobalInputs() {
-  
-  const [app, setApp] = useState("");
-  const [testCaseCollection, setTestCaseCollection] = useState("");
-  const [scenarioCollection, setScenarioCollection] = useState("");
-  const [testFlow, setTestFlow] = useState("");
-  
+export default function GlobalInputs({ setGlobalInputs }) {
+
+  const handleChange = (e) => {
+    setGlobalInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <Grid container spacing={2}>
-      <Grid item size={{ xs: 3 }}>
+      <Grid item size={{ xs: 1 }}>
         <TextField
           fullWidth
           name="app"
           label="app"
           variant="standard"
-          onChange={() => setApp(e.target.value)}
+          onChange={(e) => {
+            handleChange(e);
+          }}
         />
       </Grid>
       <Grid item size={{ xs: 3 }}>
         <TextField
           fullWidth
-          name="test-case-collection"
+          name="testCaseCollection"
           label="Test Case Collection"
           variant="standard"
-          onChange={() => setTestCaseCollection(e.target.value)}
+          onChange={(e) => {
+            handleChange(e);
+          }}
         />
       </Grid>
       <Grid item size={{ xs: 3 }}>
         <TextField
           fullWidth
-          name="test-scenario-collection"
+          name="testScenarioCollection"
           label="Test Scenario Collection"
           variant="standard"
-          onChange={() => setTestCaseCollection(e.target.value)}
+          onChange={(e) => {
+            handleChange(e);
+          }}
         />
       </Grid>
       <Grid item size={{ xs: 3 }}>
         <TextField
           fullWidth
-          name="test-flow"
+          name="testFlow"
           label="Test Flow"
           variant="standard"
-          onChange={() => setTestFlow(e.target.value)}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+      </Grid>
+      <Grid item size={{ xs: 2 }}>
+        <TextField
+          fullWidth
+          name="testClass"
+          label="Test Class"
+          variant="standard"
+          onChange={(e) => {
+            handleChange(e);
+          }}
         />
       </Grid>
     </Grid>
