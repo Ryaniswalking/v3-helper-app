5c2023c36c40879497cfda334a3777e953e6071e
Ryan Walker <rwalker+MIG@mercuryinsurance.com>
2025-05-16 10:53:04 -0700

    remove global inputs

--- src/components/testcase/TestCaseList.jsx
+++ src/components/testcase/TestCaseList.jsx
@@ -1,13 +1,7 @@
 import { Box, Grid, Typography } from "@mui/material";
-import GlobalInputs from "./GlobalInputs";
 function TestCaseList({ testCases }) {
   return (
     <Box>
-      <Grid container>
-        <Grid item size={{ xs: 12 }}>
-          <GlobalInputs />
-        </Grid>
-      </Grid>
       <div className="testcase-list">
         {testCases.map((testCase) => (
           <div key={testCase.id} className="testcase-card">
